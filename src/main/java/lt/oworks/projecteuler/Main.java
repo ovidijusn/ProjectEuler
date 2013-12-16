bf413a4384ddd3c2496109527e506e221b8e37a2
Ovidijus <ovidijus1989@gmail.com>
2013-12-16 21:46:47 +0200

    solve problem 1

---
+++ src/main/java/lt/oworks/projecteuler/Main.java
@@ -0,0 +1,17 @@
+package lt.oworks.projecteuler;
+
+import lt.oworks.projecteuler.problems.Problem1;
+
+/**
+ * @author Ovidijus
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+
+		new Problem1().solve();
+
+	}
+
+}
---
+++ src/main/java/lt/oworks/projecteuler/problems/Problem.java
@@ -0,0 +1,9 @@
+package lt.oworks.projecteuler.problems;
+
+/**
+ * @author Ovidijus
+ * 
+ */
+public abstract class Problem {
+	abstract public void solve();
+}

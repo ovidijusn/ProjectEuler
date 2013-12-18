09eb55d0f64456a61d766971d23e22616943e5bf
Ovidijus <ovidijus1989@gmail.com>
2013-12-19 00:23:11 +0200

    Summation of primes

---
+++ src/main/java/lt/oworks/projecteuler/problems/Problem10.java
@@ -0,0 +1,29 @@
+package lt.oworks.projecteuler.problems;
+
+import lt.oworks.projecteuler.Utils;
+
+/**
+ * Summation of primes
+ *
+ * @see http://projecteuler.net/problem=10
+ * @author Ovidijus
+ */
+public class Problem10 extends Problem {
+
+    private static final int LIMIT = 2_000_000;
+
+    @Override
+    public String solve() {
+
+        long sum = 2;
+
+        for (long i = 3; i < LIMIT-1; i+=2) {
+            if (Utils.isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        return Long.toString(sum);
+    }
+
+}

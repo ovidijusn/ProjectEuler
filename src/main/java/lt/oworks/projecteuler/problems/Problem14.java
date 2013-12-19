adcd4d62a7a4c2ff89d828bdc72ebd4a9d6c1368
Ovidijus <ovidijus1989@gmail.com>
2013-12-19 22:17:40 +0200

    Longest Collatz sequence

---
+++ src/main/java/lt/oworks/projecteuler/problems/Problem14.java
@@ -0,0 +1,44 @@
+package lt.oworks.projecteuler.problems;
+
+/**
+ * Longest Collatz sequence
+ *
+ * @see http://projecteuler.net/problem=14
+ * @author Ovidijus
+ */
+public class Problem14 extends Problem {
+
+    private static final int LIMIT = 1_000_000;
+
+    @Override
+    public String solve() {
+        long max = 0;
+        int result = 0;
+        for (int i = 1; i < LIMIT; i++) {
+            long len = countChainSize(i);
+            if (len > max) {
+                max = len;
+                result = i;
+            }
+        }
+
+        return Long.toString(result);
+    }
+
+    private long countChainSize(final long pNum) {
+        long result = 1;
+
+        long chain = pNum;
+        while (chain > 1) {
+            if (chain % 2 == 0) {
+                chain = chain / 2;
+            } else {
+                chain = 3 * chain + 1;
+            }
+            result++;
+        }
+
+        return result;
+    }
+
+}

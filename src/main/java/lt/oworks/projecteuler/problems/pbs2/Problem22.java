89521444120347171a8fcf71151e5e6c2e736e3d
Ovidijus <Ovidijus@192.168.1.161>
2013-12-22 18:52:30 +0200

    Names scores

---
+++ src/main/java/lt/oworks/projecteuler/problems/pbs2/Problem22.java
@@ -0,0 +1,22 @@
+package lt.oworks.projecteuler.problems.pbs2;
+
+import lt.oworks.projecteuler.problems.Problem;
+
+/**
+ * Names scores
+ *
+ * @see http://projecteuler.net/problem=22
+ * @author Ovidijus
+ */
+public class Problem22 extends Problem {
+
+    private static final long LIMIT = 10000;
+
+    @Override
+    public String solve() {
+        long sum = 0;
+
+        return Long.toString(sum);
+    }
+
+}

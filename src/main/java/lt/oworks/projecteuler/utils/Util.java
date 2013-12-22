b9ef56b8506218cd23ace06967783ba1f673fe5d
Ovidijus <Ovidijus@192.168.1.161>
2013-12-22 18:19:08 +0200

    add findDivisors method with tests

--- src/main/java/lt/oworks/projecteuler/utils/Util.java
+++ src/main/java/lt/oworks/projecteuler/utils/Util.java
@@ -1,5 +1,11 @@
 package lt.oworks.projecteuler.utils;
 
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 /**
  *
  * @author Ovidijus
@@ -19,6 +25,26 @@ public class Util {
             }
         }
 
+        return result;
+    }
+
+    public static long[] findDivisors(final long pNum) {
+
+        final double root = Math.sqrt(pNum);
+        final long limit = Math.round(root) + 1;
+
+        final Set<Long> list = new TreeSet<>();
+        for (long i = 1; i < limit; i++) {
+            if (pNum % i == 0) {
+                list.add(i);
+                list.add(pNum / i);
+            }
+        }
+        final long[] result = new long[list.size()];
+        int i = 0;
+        for (final long div : list) {
+            result[i++] = div;
+        }
         return result;
     }
 
--- src/test/java/lt/oworks/projecteuler/utils/TestUtil.java
+++ src/test/java/lt/oworks/projecteuler/utils/TestUtil.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lt.oworks.projecteuler.utils;
 
-import java.math.BigDecimal;
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
 
@@ -17,12 +11,24 @@ public class TestUtil {
 
     @Test
     public void testIsPrime() {
-
         assertFalse(Util.isPrime(1), "1 nera pirminis");
         assertTrue(Util.isPrime(2), "2 - pirminis");
         assertFalse(Util.isPrime(4), "4 - nëra pirminis");
         assertTrue(Util.isPrime(997), "997 - yra pirminis");
         assertFalse(Util.isPrime(100), "100 - nera pirminis");
+    }
+
+    @Test
+    public void testFindDivisors() {
+        long[] arr = {1};
+        assertEquals(Util.findDivisors(1), arr, "1 has only 1");
+        long[] arr2 = {1, 2};
+        assertEquals(Util.findDivisors(2), arr2, "2 has 1, 2");
+        long[] arr3 = {1, 2, 4, 5, 10, 11, 20, 22, 44, 55, 110, 220};
+        assertEquals(Util.findDivisors(220), arr3);
+        long[] arr4 = {1, 2, 4, 71, 142, 284};
+        assertEquals(Util.findDivisors(284), arr4);
+
     }
 
     public TestUtil() {

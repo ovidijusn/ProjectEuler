1539f0be15ae5cfdf9856e81e391b03ea8a48e22
Ovidijus <Ovidijus@192.168.1.161>
2013-12-22 18:51:10 +0200

    Amicable numbers

--- src/main/java/lt/oworks/projecteuler/problems/pbs2/Problem21.java
+++ src/main/java/lt/oworks/projecteuler/problems/pbs2/Problem21.java
@@ -3,6 +3,7 @@ package lt.oworks.projecteuler.problems.pbs2;
 import java.math.BigDecimal;
 import lt.oworks.projecteuler.utils.Utils;
 import lt.oworks.projecteuler.problems.Problem;
+import lt.oworks.projecteuler.utils.Util;
 
 /**
  * Amicable numbers
@@ -12,13 +13,30 @@ import lt.oworks.projecteuler.problems.Problem;
  */
 public class Problem21 extends Problem {
 
-    private static final BigDecimal INPUT = new BigDecimal(100);
+    private static final long LIMIT = 10000;
 
     @Override
     public String solve() {
-        final BigDecimal factor = Utils.factorial(INPUT);
+        long sum = 0;
+        for (long i = 2; i < LIMIT; i++) {
+            if (isAmicable(i)) {
+                sum += i;
+                System.out.println(i);
+            }
+        }
+        return Long.toString(sum);
+    }
 
-        return Utils.digitSum(factor).toString();
+    private boolean isAmicable(final long pNum) {
+        final long[] divs = Util.findDivisors(pNum);
+        final long sum = Util.addAll(divs) - divs[divs.length - 1];
+        boolean result = false;
+        if (pNum != sum) {
+            final long[] divs2 = Util.findDivisors(sum);
+            final long sum2 = Util.addAll(divs2) - divs2[divs2.length - 1];
+            result = sum2 == pNum;
+        }
+        return result;
     }
 
 }

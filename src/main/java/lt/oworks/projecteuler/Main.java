3bbabc90706fe3158638ed5901a13bd6de5c123e
Ovidijus <Ovidijus@Ovidijus-PC.home>
2013-12-26 18:01:02 +0200

    1000-digit Fibonacci number

--- src/main/java/lt/oworks/projecteuler/Main.java
+++ src/main/java/lt/oworks/projecteuler/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-        final String result = SolverFactory.solveProblem(24);
+        final String result = SolverFactory.solveProblem(25);
         System.out.println("Result is " + result);
     }
 
---
+++ src/main/java/lt/oworks/projecteuler/problems/pbs2/Problem25.java
@@ -0,0 +1,35 @@
+package lt.oworks.projecteuler.problems.pbs2;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import lt.oworks.projecteuler.problems.Problem;
+
+/**
+ * 1000-digit Fibonacci number
+ *
+ * @see http://projecteuler.net/problem=25
+ * @author Ovidijus
+ */
+public class Problem25 extends Problem {
+
+    private static final int LIMIT = 1_000;
+
+    @Override
+    public String solve() {
+
+        BigDecimal first = BigDecimal.ONE;
+        BigDecimal second = BigDecimal.ONE;
+        long n = 2;
+
+        while (second.toString().length() < LIMIT) {
+            final BigDecimal tmp = first;
+            first = second;
+            second = second.add(tmp);
+            n++;
+        }
+
+        return Long.toString(n);
+    }
+
+}

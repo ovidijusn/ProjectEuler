8e8ea2d4e9ea3a250392cff960feeca8ad87d44f
narkeovi <narkeovi@LTNARKEOVI02.lt.dom>
2014-01-14 12:59:45 +0200

    Distinct powers

--- src/main/java/lt/oworks/projecteuler/Main.java
+++ src/main/java/lt/oworks/projecteuler/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-        final String result = SolverFactory.solveProblem(28);
+        final String result = SolverFactory.solveProblem(29);
         System.out.println("Result is " + result);
     }
 

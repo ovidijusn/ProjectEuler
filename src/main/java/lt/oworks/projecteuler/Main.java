795ba0f2f315ae546ff239322beeff241cebe2be
narkeovi <narkeovi@LTNARKEOVI02.lt.dom>
2014-01-14 12:52:22 +0200

    Number spiral diagonals

--- src/main/java/lt/oworks/projecteuler/Main.java
+++ src/main/java/lt/oworks/projecteuler/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-        final String result = SolverFactory.solveProblem(27);
+        final String result = SolverFactory.solveProblem(28);
         System.out.println("Result is " + result);
     }
 

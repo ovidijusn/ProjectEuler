a8a7dd2beafd091de455f4afbd4d0d3fca345ff5
Ovidijus <ovidijus1989@gmail.com>
2014-01-16 21:49:53 +0200

    Pandigital prime Done

--- src/main/java/lt/oworks/projecteuler/Main.java
+++ src/main/java/lt/oworks/projecteuler/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-        final String result = SolverFactory.solveProblem(40);
+        final String result = SolverFactory.solveProblem(41);
 
         System.out.println("Result is " + result);
     }
---
+++ src/main/java/lt/oworks/projecteuler/problems/pbs4/Problem41.java
@@ -0,0 +1,32 @@
+package lt.oworks.projecteuler.problems.pbs4;
+
+import lt.oworks.projecteuler.problems.Problem;
+import lt.oworks.projecteuler.utils.Util;
+
+/**
+ * Pandigital prime
+ *
+ * @see http://projecteuler.net/problem=41
+ * @author Ovidijus
+ */
+public class Problem41 extends Problem {
+
+    private static final int LIMIT = 10000000;
+
+    @Override
+    public String solve() {
+
+        int rez = 0;
+        for (int i = LIMIT; i > 0; i--) {
+            final int len = ("" + i).length();
+            if (Util.isPandigital(i, len) && Util.isPrime(i)) {
+                rez = i;
+                break;
+            }
+        }
+
+        return Integer.toString(rez);
+
+    }
+
+}

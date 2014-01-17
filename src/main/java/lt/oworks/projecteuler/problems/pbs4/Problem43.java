94c2f77e7a06d48786f65fff1e70e027351759f8
Ovidijus <ovidijus1989@gmail.com>
2014-01-17 19:22:56 +0200

    Sub-string divisibility

---
+++ src/main/java/lt/oworks/projecteuler/problems/pbs4/Problem43.java
@@ -0,0 +1,23 @@
+package lt.oworks.projecteuler.problems.pbs4;
+
+import lt.oworks.projecteuler.problems.Problem;
+
+/**
+ * Sub-string divisibility
+ *
+ * @see http://projecteuler.net/problem=43
+ * @author Ovidijus
+ */
+public class Problem43 extends Problem {
+
+    @Override
+    public String solve() {
+
+        return null;
+    }
+
+    @Override
+    public String getAnswer() {
+        return null;
+    }
+}

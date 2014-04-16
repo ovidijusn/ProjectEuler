74487ace6d79cbb4b1c5daeac6e409ff4856eee4
Ovidijus <Ovidijus@Ovidijus-PC>
2014-04-16 23:32:08 +0300

    Prime permutations started

---
+++ src/main/java/lt/oworks/projecteuler/problems/pbs4/Problem49.java
@@ -0,0 +1,19 @@
+package lt.oworks.projecteuler.problems.pbs4;
+
+import lt.oworks.projecteuler.problems.Problem;
+
+/**
+ * Prime permutations
+ *
+ * @see http://projecteuler.net/problem=49
+ * @author Ovidijus
+ */
+public class Problem49 extends Problem {
+
+    @Override
+    public String solve() {
+
+        return null;
+    }
+
+}

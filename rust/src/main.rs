01b9318ac5ac172c35ee2b3d0249a219f1d55d08
Ovidijus Narkevicius <ovidijus@Ovidijuss-MacBook-Air.local>
2022-10-26 00:40:10 +0300

    solve 59

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    solve_59();
+}
+
+fn solve_59() {
+    let file_path = "files/p059_cipher.txt";
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    for c1 in 97..=122 {
+        for c2 in 97..=122 {
+            for c3 in 97..=122 {
+                let key: [u8; 3] = [c1, c2, c3];
+                let text = decrypt(key, &contents);
+                if text.contains("the ") && text.contains("to ") {
+                    println!("{}", text);
+                    let sum: i32 = text.as_bytes().iter().map(|c| i32::from(*c)).sum();
+                    println!("Sum of original text ASCII values: {}", sum);
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn decrypt(key: [u8; 3], contents: &String) -> String {
+    return contents
+        .split(",")
+        .map(|s| s.parse::<u8>().unwrap())
+        .enumerate()
+        .map(|(i, c)| (c ^ key[i % 3]) as char)
+        .collect();
 }
